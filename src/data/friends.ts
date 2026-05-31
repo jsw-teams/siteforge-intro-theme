fc662b60293c9dba9d94cec29b245037dc0846a2
jsw-teams <228026986+jsw-teams@users.noreply.github.com>
2026-05-31 12:52:51 +0800

    Add JS.Gripe friend links

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -5,4 +5,25 @@ export type FriendLink = {
   lang?: string;
 };
 
-export const friends: FriendLink[] = [];
+export const friends: FriendLink[] = [
+  {
+    name: 'Files.js.gripe',
+    url: 'https://files.js.gripe/',
+    description: 'Public file service for JS.Gripe.'
+  },
+  {
+    name: 'SSL Signer',
+    url: 'http://ssl-signer.js.gripe/',
+    description: 'Certificate signing and SSL utility service for JS.Gripe.'
+  },
+  {
+    name: 'Acount.js.gripe',
+    url: 'https://acount.js.gripe/',
+    description: 'Account service for JS.Gripe.'
+  },
+  {
+    name: 'Search.js.gripe',
+    url: 'https://search.js.gripe/',
+    description: 'Search service for JS.Gripe.'
+  }
+];
